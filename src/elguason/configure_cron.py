37a7de26c493066bf4d2fbcf53cc909238529297
Nahuel Ambrosini <ambro17.1@gmail.com>
2021-11-06 15:24:10 -0300

    🔎 Add logging to cron config

--- src/elguason/configure_cron.py
+++ src/elguason/configure_cron.py
@@ -2,6 +2,7 @@ import sys
 from pathlib import Path
 
 from crontab import CronTab
+from loguru import logger
 
 
 def configure(hour, spec, outputpath):
@@ -12,7 +13,14 @@ def configure(hour, spec, outputpath):
 
     with CronTab(user=True) as cron:
         # Find or create cron
-        job = next((x for x in cron.find_command(micontador)), cron.new(command=new_command))
+        existing_job = next((x for x in cron.find_command(micontador)), None)
+        if existing_job:
+            logger.info("Updating existing cron")
+            job = existing_job
+        else:
+            logger.info("Creating new cron")
+            job = cron.new(command=new_command)
+
         job.command = new_command
         job.setall(f'0 {hour} * * *')  # Set cron to run on hour:00 minutes every day
         assert job.is_valid()
