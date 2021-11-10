d2e6036ea238993ef5b017ba36e632b6015d7297
Nahuel Ambrosini <ambro17.1@gmail.com>
2021-11-10 14:35:37 -0300

    🔧 Refactor to use normal distribution to generate a distribution from an expected mean

--- src/elguason/planificar.py
+++ src/elguason/planificar.py
@@ -2,7 +2,8 @@ import calendar
 import csv
 import datetime
 import random
-
+import statistics
+from typing import List
 
 FACTURACION_ANUAL_MONOTRIBUTO_POR_CATEGORIA = {
     'A': 370_000,
@@ -25,40 +26,47 @@ def facturacion_mensual_por_categoria():
         print(f'Categoria {cat}', f'${fact}')
 
 
-facturacion_mensual_por_categoria()
-# Ver si podes ingresar categoria monotributo directamente
-gastos_mensuales = int(input("Gastos Mensuales: ") or 150_000)
-por_dia = gastos_mensuales / 20
-facturacion_por_dia_randomized = por_dia + random.randint(100, 2000) - random.randint(200, 400)
-print(f'Deberias facturar {por_dia} por dia habil para justificar esos gastos')
-
-today = datetime.datetime.today()
-first, last = calendar.monthrange(today.year, today.month)
-monthdays = [datetime.date(today.year, today.month, daynum) for daynum in range(1, last + 1)]
-future_weekdays = [x for x in monthdays if x.day >= today.day and x.weekday() not in (5, 6)]
-dias_a_facturar = len(future_weekdays)
-por_dia = gastos_mensuales / dias_a_facturar
+def prompt():
+    # Ver si podes ingresar categoria monotributo directamente
+    gastos_mensuales = int(input("Gastos Mensuales: ") or 150_000)
+    today = datetime.datetime.today()
+    first, last = calendar.monthrange(today.year, today.month)
+    monthdays = [datetime.date(today.year, today.month, daynum) for daynum in range(1, last + 1)]
+    future_weekdays = [x for x in monthdays if x.day >= today.day and x.weekday() not in (5, 6)]
+    dias_a_facturar = len(future_weekdays)
+    por_dia = gastos_mensuales / dias_a_facturar
+    return future_weekdays, por_dia
 
 
 def num_per_day_with_offset(num, limit=12500):
     return int(min(num + random.randint(-2500, 2500), limit - random.randint(100, 1000)))
 
 
-with open('planificacion.csv', 'w') as f:
-    writer = csv.writer(f)
-    header = ['fecha', 'servicio', 'monto', 'cuit_destino', 'punto_de_venta']
-    writer.writerow(header)
-    total = 0
-    for day in future_weekdays:
-        monto = num_per_day_with_offset(por_dia)
-        total += monto
-        row = [day.strftime('%d/%m/%Y'), 'Servicios Profesionales', monto, '', '']
-        writer.writerow(row)
+def write(future_weekdays: List[datetime.date], facturacion_por_dia):
+    with open('planificacion.csv', 'w') as f:
+        writer = csv.writer(f)
+        header = ['fecha', 'servicio', 'monto', 'cuit_destino', 'punto_de_venta']
+        writer.writerow(header)
+        total = 0
 
-    print(f'La planificacion emitirá {dias_a_facturar} facturas por un monto total de {total}')
+        normal_dist_from_facturacion_por_dia = statistics.NormalDist(mu=facturacion_por_dia, sigma=2000)
+        amounts = normal_dist_from_facturacion_por_dia.samples(n=len(future_weekdays))
+
+        for day, amount in zip(future_weekdays, amounts):
+            total += amount
+            row = [day.strftime('%d/%m/%Y'), 'Servicios Profesionales', amount, '', '']
+            writer.writerow(row)
+
+        print(f'La planificacion emitirá {len(future_weekdays)} facturas por un monto total de {total}')
 
 
-with open('planificacion.csv', 'r') as f:
-    reader = csv.reader(f)
-    for row in reader:
-        print(row)
+def read():
+    with open('planificacion.csv', 'r') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            print(row)
+
+
+weekdays, pordia = prompt()
+write(weekdays, pordia)
+read()
