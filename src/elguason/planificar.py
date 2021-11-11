4d662043355c1d77062f31a2ac8ae238eed4a662
Nahuel Ambrosini <ambro17.1@gmail.com>
2021-11-10 21:50:52 -0300

    ✨ Add script to design billing plan

--- src/elguason/planificar.py
+++ src/elguason/planificar.py
@@ -1,11 +1,11 @@
 import calendar
 import csv
 import datetime
-import random
 import statistics
-from typing import List
+from typing import List, Iterable, Tuple
 
 FACTURACION_ANUAL_MONOTRIBUTO_POR_CATEGORIA = {
+    # See https://www.afip.gob.ar/monotributo/categorias.asp
     'A': 370_000,
     'B': 550_000,
     'C': 770_000,
@@ -21,52 +21,39 @@ FACTURACION_MENSUAL_MONOTRIBUTO_POR_CATEGORIA = {
 }
 
 
-def facturacion_mensual_por_categoria():
-    for cat, fact in FACTURACION_MENSUAL_MONOTRIBUTO_POR_CATEGORIA.items():
-        print(f'Categoria {cat}', f'${fact}')
+def generar_plan_de_facturacion(gastos_mensuales) -> List[Tuple[datetime.date, float]]:
+    billable_days: List[datetime.date] = generate_billable_days()
+    montos_por_dia = generar_facturacion_por_dia(gastos_mensuales, billable_days)
+    return montos_por_dia
 
 
-def prompt():
-    # Ver si podes ingresar categoria monotributo directamente
-    gastos_mensuales = int(input("Gastos Mensuales: ") or 150_000)
+def generate_billable_days():
+    """A day is billable if it belongs to a working day in the future the same than today """
     today = datetime.datetime.today()
     first, last = calendar.monthrange(today.year, today.month)
     monthdays = [datetime.date(today.year, today.month, daynum) for daynum in range(1, last + 1)]
     future_weekdays = [x for x in monthdays if x.day >= today.day and x.weekday() not in (5, 6)]
-    dias_a_facturar = len(future_weekdays)
-    por_dia = gastos_mensuales / dias_a_facturar
-    return future_weekdays, por_dia
+    return future_weekdays
 
 
-def num_per_day_with_offset(num, limit=12500):
-    return int(min(num + random.randint(-2500, 2500), limit - random.randint(100, 1000)))
+def generar_facturacion_por_dia(
+        gastos_mensuales, billable_days: List[datetime.date]
+) -> List[Tuple[datetime.date, float]]:
+    """Dada una distribucion normal, genera cant_dias muestras respetando esa dist. normal"""
+    cant_dias = len(billable_days)
+    facturacion_por_dia = gastos_mensuales / cant_dias
+    normal_dist_from_facturacion_por_dia = statistics.NormalDist(mu=facturacion_por_dia, sigma=1500)
+    amounts = normal_dist_from_facturacion_por_dia.samples(n=cant_dias)
+    return list(zip(billable_days, [int(x) for x in amounts]))
 
 
-def write(future_weekdays: List[datetime.date], facturacion_por_dia):
-    with open('planificacion.csv', 'w') as f:
+def write_plan(bill_by_day, path='planificacion.csv'):
+    with open(path, 'w') as f:
         writer = csv.writer(f)
         header = ['fecha', 'servicio', 'monto', 'cuit_destino', 'punto_de_venta']
         writer.writerow(header)
-        total = 0
-
-        normal_dist_from_facturacion_por_dia = statistics.NormalDist(mu=facturacion_por_dia, sigma=2000)
-        amounts = normal_dist_from_facturacion_por_dia.samples(n=len(future_weekdays))
-
-        for day, amount in zip(future_weekdays, amounts):
-            total += amount
+        for day, amount in bill_by_day:
             row = [day.strftime('%d/%m/%Y'), 'Servicios Profesionales', amount, '', '']
             writer.writerow(row)
 
-        print(f'La planificacion emitirá {len(future_weekdays)} facturas por un monto total de {total}')
-
-
-def read():
-    with open('planificacion.csv', 'r') as f:
-        reader = csv.reader(f)
-        for row in reader:
-            print(row)
-
-
-weekdays, pordia = prompt()
-write(weekdays, pordia)
-read()
+    return path
