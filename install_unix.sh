bc077ef2bdac7ac1acbe52ba91370fd3b4a363aa
Ambro17 <ambro17.1@gmail.com>
2021-11-11 23:21:18 -0300

    ⏫ Upgrade pip and install with it instead of flit. Latest pip versions can already install in editable mode from toml. See https://pip.pypa.io/en/stable/news/

--- install_unix.sh
+++ install_unix.sh
@@ -1,5 +1,5 @@
 python3 -m venv venv && \
-./venv/bin/pip install --upgrade pip flit && \
-./venv/bin/python -m flit install && \
+./venv/bin/pip install --upgrade pip && \
+./venv/bin/python -m pip install -e . && \
 ./venv/bin/python -m playwright install chromium && \
 . ./venv/bin/activate
