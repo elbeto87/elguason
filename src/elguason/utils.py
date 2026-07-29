99a069f65e83752075dca547ef7434782f391e0d
Ezequiel Gresia <gresia.ezequiel@gmail.com>
2026-07-29 14:59:30 -0300

    refactor

--- src/elguason/utils.py
+++
@@ -1,9 +0,0 @@
-from typing import Optional
-import datetime
-
-
-def parse_date(date: Optional[str]) -> datetime.date:
-    if not date:
-        return
-
-    return datetime.datetime.strptime(date, '%d/%m/%Y').date()
